6ab58e7f47ad60f480ed66d22c689bddff1d3ed1
Fr4nk1in <fushen@mail.ustc.edu.cn>
2024-09-13 09:47:00 +0000

    release: 0.0.1

--- setup.py
+++ setup.py
@@ -2,16 +2,20 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-  name = "zfp",
-  include_dirs = ["./custom/include"],
-  ext_modules = [
-    CUDAExtension(
-      name = "zfp",
-      sources = ["./custom/lib/zfp_compresser.cpp"],
-      include_dirs = ["./custom/include"],
-      libraries = ["zfp"],
-      extra_compile_args = ["-std=c++17", "-DBUILD_PYEXT", "-fvisibility=hidden"],
-    )
-  ],
-  cmdclass={"build_ext": BuildExtension}
+    name="zfp_torch",
+    author="Fr4nk1in",
+    author_email="fushen@mail.ustc.edu.cn",
+    url="https://github.com/Fr4nk1inCs/zfp-torch",
+    version="0.0.1",
+    include_dirs=["./custom/include"],
+    ext_modules=[
+        CUDAExtension(
+            name="zfp_torch",
+            sources=["./custom/lib/zfp_compresser.cpp"],
+            include_dirs=["./custom/include"],
+            libraries=["zfp"],
+            extra_compile_args=["-std=c++17", "-DBUILD_PYEXT", "-fvisibility=hidden"],
+        )
+    ],
+    cmdclass={"build_ext": BuildExtension},
 )
