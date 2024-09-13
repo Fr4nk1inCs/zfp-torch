e5abf61c1745b0a10a99414038c0ebe2ae2ad317
Fr4nk1in <fushen@mail.ustc.edu.cn>
2024-09-13 09:47:18 +0000

    fix: tests' import module name

--- test/test-1d.py
+++ test/test-1d.py
@@ -7,43 +7,44 @@ import zfpy
 DEBUG = True
 RATE = 8
 
+
 def debug(*args):
-  if DEBUG:
-    print(*args)
+    if DEBUG:
+        print(*args)
+
 
 if __name__ == "__main__":
-  if os.getenv("NODEBUG", "0") == "1":
-    DEBUG = False
+    if os.getenv("NODEBUG", "0") == "1":
+        DEBUG = False
 
-  np.set_printoptions(linewidth=np.nan)
-  torch.set_printoptions(linewidth=200)
+    np.set_printoptions(linewidth=np.nan)
+    torch.set_printoptions(linewidth=200)
 
-  debug("ZFP Compress Rate: ", RATE)
+    debug("ZFP Compress Rate: ", RATE)
 
-  # Numpy + zfpy
-  input = np.random.rand(10).astype(np.float32)
-  compressed_data = zfpy.compress_numpy(input, rate=RATE)
-  decompressed = zfpy.decompress_numpy(compressed_data)
+    # Numpy + zfpy
+    input = np.random.rand(10).astype(np.float32)
+    compressed_data = zfpy.compress_numpy(input, rate=RATE)
+    decompressed = zfpy.decompress_numpy(compressed_data)
 
-  debug("=============== Numpy + zfpy ===============")
-  debug("Input       :", input)
-  debug("Decompressed:", decompressed)
+    debug("=============== Numpy + zfpy ===============")
+    debug("Input       :", input)
+    debug("Decompressed:", decompressed)
 
-  compresser = zfp.ZFPCompresser(RATE)
-  # PyTorch + zfp w/ CUDA
-  input = torch.tensor(input, dtype=torch.float32, device='cuda').contiguous()
-  data = compresser.compress(input)
-  decompressed = compresser.decompress(data)
+    compresser = zfp.ZFPCompresser(RATE)
+    # PyTorch + zfp w/ CUDA
+    input = torch.tensor(input, dtype=torch.float32, device="cuda").contiguous()
+    data = compresser.compress(input)
+    decompressed = compresser.decompress(data)
 
-  debug("=========== PyTorch + zfp (CUDA) ===========")
-  debug("Input       :", input)
-  debug("Decompressed:", decompressed)
-  # PyTorch + zfp w/o CUDA
-  input = input.cpu().contiguous()
-  data = compresser.compress(input)
-  decompressed = compresser.decompress(data)
+    debug("=========== PyTorch + zfp (CUDA) ===========")
+    debug("Input       :", input)
+    debug("Decompressed:", decompressed)
+    # PyTorch + zfp w/o CUDA
+    input = input.cpu().contiguous()
+    data = compresser.compress(input)
+    decompressed = compresser.decompress(data)
 
-  debug("=========== PyTorch + zfp (CPU) ===========")
-  debug("Input       :", input)
-  debug("Decompressed:", decompressed)
-  
+    debug("=========== PyTorch + zfp (CPU) ===========")
+    debug("Input       :", input)
+    debug("Decompressed:", decompressed)
