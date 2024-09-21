2a2069411ae34d9465e2b4b541101d2fb1426fff
haiqwa <whq1516@mail.ustc.edu.cn>
2024-09-21 13:03:47 +0000

    fix: (de)compression without writing metadata

--- zfp_torch/lib/compress.cpp
+++ zfp_torch/lib/compress.cpp
@@ -12,23 +12,28 @@ torch::Tensor compress(const torch::Tensor &input, long rate, bool write_meta) {
   size_t bufsize = zfp_stream_maximum_size(zfp, field);
   LOGGER << "maximum buffer size: " << bufsize << std::endl;
 
+  if (write_meta)
+    bufsize += meta.byte_size();
+
   torch::Tensor output = torch::empty(
-      {static_cast<long>(bufsize + meta.byte_size())},
+      {static_cast<long>(bufsize)},
       torch::TensorOptions().device(input.device()).dtype(torch::kUInt8),
       torch::MemoryFormat::Contiguous);
 
-  void *meta_ptr = output.data_ptr();
-  void *data_ptr = static_cast<void *>(static_cast<std::byte *>(meta_ptr) +
-                                       meta.byte_size());
+  void *data_ptr = static_cast<void *>(output.data_ptr());
 
-  meta.write(meta_ptr, output.device());
+  if (write_meta) {
+    meta.write(data_ptr, output.device());
+    data_ptr = static_cast<void *>(static_cast<std::byte *>(data_ptr) +
+                                   meta.byte_size());
+  }
 
   bitstream *bitstream = stream_open(data_ptr, bufsize);
   zfp_stream_set_bit_stream(zfp, bitstream);
   zfp_stream_rewind(zfp);
 
   size_t size = zfp_compress(zfp, field);
-  LOGGER << "size after compression: " << size << std::endl;
+  LOGGER << "size after compression (without metadata): " << size << std::endl;
 
   zfp_field_free(field);
   zfp_stream_close(zfp);
@@ -39,14 +44,15 @@ torch::Tensor compress(const torch::Tensor &input, long rate, bool write_meta) {
 
 torch::Tensor decompress(const torch::Tensor &input,
                          std::optional<const Metadata> meta) {
+  void *data_ptr;
   if (meta.has_value()) {
     LOGGER << "using provided metadata" << std::endl;
+    data_ptr = input.data_ptr();
   } else {
     meta.emplace(Metadata::read(input.data_ptr(), input.device()));
+    data_ptr = static_cast<void *>(static_cast<std::byte *>(input.data_ptr()) +
+                                   meta->byte_size());
   }
-
-  void *data_ptr = static_cast<void *>(
-      static_cast<std::byte *>(input.data_ptr()) + meta->byte_size());
 
   auto output = meta->to_empty_tensor(input.device());
   auto [field, zfp] = meta->to_zfp(output);
